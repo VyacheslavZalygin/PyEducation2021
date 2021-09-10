c3ae66f618ff4ad214f9adff635fe3c1b273767f
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2021-09-10 17:38:52 +0300

    update task4 solution

--- NumberTheory/task4.py
+++ NumberTheory/task4.py
@@ -1,38 +1,24 @@
 import sys
 
-def euclidean(a, b):
+def solve(a, A, b, B, n):
     while b != 0:
+        k = (a // b)
         a, b = b, a % b
-    return abs(a)
+        A, B = B, A - k * B
+    return A%n
 
-def func(a, A, n):
-    a %= n
-    A %= n
-    b = a
-    a = n
-    c0 = 0
-    while b != 0:
-        k = -(a // b)
-        a, b = b, a % b
-        c0, A = A, c0 + A * k
-    if c0 % n < abs(c0): c0 = c0 % n
-    return c0
-
-def try_solve(x, a, A, n):
-    return (x*a)%n, A%n
+def is_fits(c, a, A, n):
+    return (c*a)%n == A%n
 
 def main():
     a, A, b, B, n = [int(x) for x in sys.stdin.read().split()]
+    a %= n
     A %= n
+    b %= n
     B %= n
-    c = func(a, A, n)
-    d = func(b, B, n)
-    print('x = ' + str(n) + 'k + ' + str(c))
-    print('x = ' + str(n) + 'k + ' + str(d))
-    for k in range(-100, 100):
-        x = k*n + c
-        y = k*n + d
-        print(x, y)
+    c = solve(a, A, b, B, n)
+    if is_fits(c, a, A, n) and is_fits(c, b, B, n):
+        print(c)
 
 main()
 
---
+++ NumberTheory/task8.py
@@ -0,0 +1,7 @@
+import sys
+
+def main():
+    a, b = [int(x) for x in sys.stdin.read().split()]
+
+
+main()
