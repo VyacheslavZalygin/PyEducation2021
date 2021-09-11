1a5a719a3fa253d884f8871594c97ec227799336
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2021-09-11 18:17:34 +0300

    add task8

--- NumberTheory/task8.py
+++ NumberTheory/task8.py
@@ -1,7 +1,17 @@
 import sys
 
+class Arr:
+    def __init__(self, a, b):
+        self.arr = [True for _ in range(a-b)]
+        self.a = a
+        self.b = b
+
+    def __getitem__(self, item):
+        return self.arr[int(item)-self.a]
+
 def main():
     a, b = [int(x) for x in sys.stdin.read().split()]
-
+    arr = Arr(a, b)
+    print(arr[6])
 
 main()
