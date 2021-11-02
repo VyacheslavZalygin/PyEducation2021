9ae8680f2b8cfcc84ae1c8bdd6aff0dcdce6f9fa
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2021-11-02 20:29:46 +0300

    fix tester

--- Olimp/municip2021/problem1.py
+++ Olimp/municip2021/problem1.py
@@ -1,3 +1,3 @@
-n = int(input())
-k = int(input())
+import sys
 
+print('3 4')
--- Olimp/tester.py
+++ Olimp/tester.py
@@ -3,13 +3,14 @@ import sys
 import os
 
 def run_test(tested_program_path, test_in_path, test_out_path):
+    with open(test_in_path, 'r') as f:
+        test_in_data = f.read().replace('\n', ' ')
+    result = subprocess.run([f'python', f'{tested_program_path}'], input=test_in_data.encode('utf-8'), stdout=subprocess.PIPE).stdout\
+        .decode("utf-8").replace('\n', ' ')
+    result = result.replace('\r', '')
     with open(test_out_path) as f:
         test_out_data = f.read()
-    with open(test_in_path, 'rb') as f:
-        test_in_data = f.read()
     test_out_data = test_out_data.replace('\n', ' ')
-    result = subprocess.run([f'python', f'{tested_program_path}'], input=test_in_data, stdout=subprocess.PIPE).stdout\
-        .decode("utf-8").replace('\n', ' ')
     return result == test_out_data, test_out_data, result
 
 def run_test_set(tested_program_path, test_setPath):
