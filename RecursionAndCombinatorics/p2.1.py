7d71303d6f4a6bde964b5d4e23888860b7ebdcdd
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2021-11-19 12:07:47 +0300

    p2.1

---
+++ RecursionAndCombinatorics/p2.1.py
@@ -0,0 +1,15 @@
+import sys
+
+def div(a, e = None):
+  return {x for x in a if x != e}
+
+def sequences(length, free):
+  if length == 0:
+    return [[]]
+  return [[head, *tail]
+          for head in free
+          for tail in sequences(length - 1, div(free, head))]
+
+l, n = [int(x) for x in sys.stdin.read().split()]
+for s in sequences(l, {x for x in range(1, n + 1)}):
+  print(*s)
