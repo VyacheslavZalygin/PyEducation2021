46be99b89270482ec7e9fc0f8399ca5a4dde9420
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2021-11-30 12:39:31 +0300

    p4.1

---
+++ RecursionAndCombinatorics/p4.1.py
@@ -0,0 +1,25 @@
+import sys
+
+def options(length, number):
+  if length == 1:
+    return [number]
+  return [x for x in range(1, number-length+2)]
+
+def sequence(length, number):
+	if length == 0: 
+		return [[]]
+	return [[head, *tail]
+				 for head in options(length, number)
+				 for tail in sequence(length-1, number-head)]
+
+l, n = [int(x) for x in sys.stdin.read().split()]
+for s in sequence(l, n):
+	print(*s)
+
+# 3 5
+# 1 1 3
+# 1 2 2
+# 1 3 1
+# 2 1 2
+# 2 2 1
+# 3 1 1
