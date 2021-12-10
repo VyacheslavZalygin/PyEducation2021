b2a87156872cc08f40f6bd3607b9eb268b6adba0
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2021-12-10 10:45:18 +0300

    p4

--- RecursionAndMemoization/p1.py
+++ RecursionAndMemoization/p1.py
@@ -3,6 +3,16 @@ import sys
 def f(n):
   if n == 1:
     return 1
+  if n == 2:
+    return 2
+  if n == 3:
+    return 2
+  if n == 4:
+    return 5
+  if n == 5:
+    return 5
+  if n % 2 == 1:
+    return f(n-1)
   return f(n-1)*2
 
 n = [int(x) for x in sys.stdin.read().split()][0]
--- RecursionAndMemoization/p4.py
+++ RecursionAndMemoization/p4.py
@@ -4,15 +4,15 @@ table = {}
 
 def memo(k, f):
   if k not in table:
-    table[k] = f(k)
+    table[k] = f(*k)
   return table[k]
 
-def count(n):
-  if n == 1:
+def count(s, n):
+  if s == n:
     return 1
-  elif n < 1:
+  elif s > n:
     return 0
-  return memo(n-1, count) + memo(n-2, count) + memo(n-3, count)
+  return memo((s+1, n), count) + memo((s+2, n), count) + memo((s*3, n), count)
 
 b = [int(x) for x in sys.stdin.read().split()][0]
-print(count(b))
+print(count(1, b))
