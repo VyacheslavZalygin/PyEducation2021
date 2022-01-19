4e705f6877e5364b893976166c768adb30120031
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-01-19 10:59:41 +0300

    inf and moving files

---
+++ Inf/prime_numbers.py
@@ -0,0 +1,15 @@
+def divisors(n):
+    d = 1
+    ds = []
+    while d*d <= n:
+        if n % d == 0:
+            ds.append(d)
+            if d*d != n:
+                ds.append(n//d)
+        d += 1
+    return ds
+
+def is_prime(n):
+    return len(divisors(n)) == 2
+
+print(is_prime(5))
