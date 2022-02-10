8217e2e11f03642dbb2ab930f5094ac92ade6376
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-02-10 20:36:08 +0300

    p10

--- RecursionAndMemoization/p10.py
+++ RecursionAndMemoization/p10.py
@@ -4,5 +4,13 @@ T = sys.stdin.read().strip()
 a = []
 for i in range(len(T)+1):
     a.append((T[i:], i))
-a.sort()
+
+i = 1
+while True:
+    b = a[:]
+    b.sort(key=lambda s: s[:min(i, len(s))])
+    if a == b: break
+    a = b
+    i *= 2
+
 print(*[p[1] for p in a])
