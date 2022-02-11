265fff29893b0ffad3584eaf2e92da7ee3e502dc
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-02-11 13:46:23 +0300

    p6 naive algorithm

--- RecursionAndMemoization/p6.py
+++ RecursionAndMemoization/p6.py
@@ -5,22 +5,25 @@ import sys
 # считает число Гранди для вершины графа
 def grundy_function(table, v):
 	neighbor_set = set([grundy_function(table, n) for n in table[v]])
-	
+
 	result = 0	
 	while result in neighbor_set: result += 1
 	return result
 
 # строит граф
-def solve(vs):
-    a = []
-    a = [[(*vs[:i],  *((j, v-j-2) if j != 0 else (v-j-2,)), *vs[i+1:]) 
-            for j in range(0, (v-2)//2+1)] for i, v in enumerate(vs)]
-    b = set()
-    for n_vs in a:
-        b.add(solve(n_vs))
-    res = 0
-    while res in b: res += 1
-    return res
+def solve(vs, table=None):
+    if table is None:
+        table = {}
+    table[vs] = []
+    for i, v in enumerate(vs):
+        table[vs].extend([(
+            *vs[:i], 
+            *((j, v-j-2) if j != 0 else (v-j-2,)), 
+            *vs[i+1:]) 
+                for j in range(0, (v-2)//2+1)])
+    for n_vs in table[vs]:
+        solve(n_vs, table) 
+    return table
 
 N = (int(sys.stdin.read()),)
-print(solve(N))
+print(grundy_function(solve(N), N)) 
