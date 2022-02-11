c88bbeb63952f3063ce09ab1571f26cd890fee87
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-02-11 15:32:34 +0300

    p6 works

--- RecursionAndMemoization/p6.py
+++ RecursionAndMemoization/p6.py
@@ -1,29 +1,38 @@
 import sys
 
-# сделать граф состояний, по нему сделать разметку Гранди. Потом граф упразднить
+# Вычислять числа grundy от 0 до n, строя граф, пока не замечаем значение, которое уже знаем
 
-# считает число Гранди для вершины графа
-def grundy_function(table, v):
-	neighbor_set = set([grundy_function(table, n) for n in table[v]])
+# def grundy(v):
+#     b = set()
+#     for i, n in enumerate(v): # для каждого кусочка
+#         for j in range(0, (n-2)//2+1): # все варианты перестановок
+#             b.add(grundy((*v[:i], *((j, n-j-2) if j != 0 else (n-j-2,)), *v[i+1:])))
+#     res = 0
+#     while res in b: res += 1
+#     return res
 
-	result = 0	
-	while result in neighbor_set: result += 1
-	return result
+# cached = grundy
+# cache = {}
+# def grundy(v):
+#     if v not in cache:
+#         cache[v] = cached(v)
+#     return cache[v]
 
-# строит граф
-def solve(vs, table=None):
-    if table is None:
-        table = {}
-    table[vs] = []
-    for i, v in enumerate(vs):
-        table[vs].extend([(
-            *vs[:i], 
-            *((j, v-j-2) if j != 0 else (v-j-2,)), 
-            *vs[i+1:]) 
-                for j in range(0, (v-2)//2+1)])
-    for n_vs in table[vs]:
-        solve(n_vs, table) 
-    return table
+def grundy(n):
+    if n < 1: return 0
+    targets = set()
+    for x in range((n-2)//2+1):
+        targets.add(grundy(x) ^ grundy(n-x-2))
+    res = 0
+    while res in targets: res += 1
+    return res
 
-N = (int(sys.stdin.read()),)
-print(grundy_function(solve(N), N)) 
+cached = grundy
+cache = {}
+def grundy(n):
+    if n not in cache:
+        cache[n] = cached(n)
+    return cache[n]
+
+N = int(sys.stdin.read())
+print(2 if grundy(N) == 0 else 1)
