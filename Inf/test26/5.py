66a46b63523cdfa24d6d1dac09e07f48532ee9e8
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-03-01 18:09:17 +0300

    5 failed

--- Inf/test26/5.py
+++ Inf/test26/5.py
@@ -7,21 +7,22 @@ res_sum = sum(special)
 s -= res_sum
 p = sorted([x for x in p if not(200 <= x <= 210)])
 
-def value_or_default(a, i, d):
-  return a[i] \
-    if (i >= 0 and len(a) >= i+1) or (i < 0 and len(a) >= -i ) \
-    else d
+i = 0
+while i < len(p) and s-p[i] >= 0:
+  s -= p[i]
+  res_sum += p[i]
+  count += 1
+  i += 1
+i -= 1
 
-def f(m, p):
-  if m == 0: return []
-  return value_or_default(sorted(
-    [[e, *f(m-e, p[:i] + p[i+1:])]for i, e in enumerate(p) if m - e >= 0],
-    key=lambda arr:
-      (len(arr),
-       value_or_default(arr, -1, 0),
-       value_or_default(arr, -2, 0))
-    ), -1, [])
-
-res = f(s, p)
-print(len(res)+count, sum(res)+res_sum)
+j = 0
+if i+1 < len(p):
+  while s >= 0 and i+j+1 < len(p):
+    s += p[j]
+    res_sum -= p[j]
+    j += 1
+    s -= p[i + j]
+    res_sum += p[i+j]
+  j -= 1
+print(count, res_sum)
 # 6 605 140 205 120 160 100 340
---
+++ Inf/test26/p5.1.py
@@ -0,0 +1,28 @@
+import sys
+
+_, s, *p = [int(x) for x in sys.stdin.read().split()]
+special = [x for x in p if 200 <= x <= 210]
+count = len(special)
+res_sum = sum(special)
+s -= res_sum
+p = sorted([x for x in p if not(200 <= x <= 210)])
+
+def value_or_default(a, i, d):
+  return a[i] \
+    if (i >= 0 and len(a) >= i+1) or (i < 0 and len(a) >= -i ) \
+    else d
+
+def f(m, p):
+  if m == 0: return []
+  return value_or_default(sorted(
+    [[e, *f(m-e, p[:i] + p[i+1:])]for i, e in enumerate(p) if m - e >= 0],
+    key=lambda arr:
+      (len(arr),
+       value_or_default(arr, -1, 0),
+       value_or_default(arr, -2, 0))
+    ), -1, [])
+
+res = f(s, p)
+print(res)
+print(len(res)+count, sum(res)+res_sum)
+# 6 605 140 205 120 160 100 340
