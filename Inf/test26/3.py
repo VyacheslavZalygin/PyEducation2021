e980e7b807cef332f9e1b765b624b31fc2cb46e6
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-03-01 16:49:45 +0300

    3 4

--- Inf/test26/3.py
+++ Inf/test26/3.py
@@ -2,12 +2,27 @@ import sys
 
 s, *p = [x for x in sys.stdin.read().split()]
 s = int(s)
-p = [(int(p[i]), p[i+1]) for i in range(0, len(p), 2)]
-p = sorted(p)
-print(p)
+p = [(int(p[i]), int(p[i+1]), p[i+2]) for i in range(0, len(p), 3)]
+table = { 'A': [], 'B': []}
+for (price, amount, t) in p:
+  table[t].append([price, amount])
 
-def f(a):
-  r = [[type, *f(a-price)] for (price, type) in p if a - price > 0]
-  return max(r, key=lambda x: x.count('A'))
+def B(): return table['B']
+def A(): return table['A']
 
-print(f(s))
+table['A'].sort()
+table['B'].sort()
+while B() != [] and s-B()[0][0] > 0:
+  s -= B()[0][0]
+  B()[0][1] -= 1
+  if B()[0][1] == 0:
+    B().pop(0)
+
+count = 0
+while A() != [] and s-A()[0][0] > 0:
+  s -= A()[0][0]
+  A()[0][1] -= 1
+  count += 1
+  if A()[0][1] == 0:
+    A().pop(0)
+print(count, s)
