b3d059a568741e9b4a2118eb280c76c6c6eaa864
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-04-01 12:10:08 +0300

    2 works

--- BasedOnThe27th/2.py
+++ BasedOnThe27th/2.py
@@ -1,38 +1,31 @@
 import sys
 
-div = 3
-# data = []
-# for string in sys.stdin.read().strip().split('\n'):
-#     if string == "": continue
-#     table = [None for _ in range(div)]
-#     for row in string.split():
-#         row = int(row)
-#         key = row % div
-#         if table[key] == None:
-#             table[key] = row
-#         else:
-#             table[key] = max(row, table[key])
-#     data.append(table)
+def max_with_none(*a):
+    a = [x for x in a if x != None]
+    return max(a) if len(a) != 0 else None
 
-# print(data)
+div = 15
+data = []
+for string in sys.stdin.read().strip().split('\n'):
+    if string == "": continue
+    table = [None for _ in range(div)]
+    for row in string.split():
+        row = int(row)
+        key = row % div
+        if table[key] == None:
+            table[key] = row
+        else:
+            table[key] = max(row, table[key])
+    data.append(table)
 
-# m_data = [None for _ in range(div)]
-m_data = [None, None, 5]
-data = [[6, 7, 14]]
+m_data = [0] + [None for _ in range(div-1)]
 for string in data:
-    for j, m in enumerate(m_data[:]):
-        for i, num in enumerate(string):
-            if num == None: continue
-            if m == None:
-                m_data[i] = num
-            else:
+    curr_data = [None for _ in range(div)]
+    for j, m in enumerate(m_data):
+        if m != None:
+            for i, curr in enumerate(string):
+                if curr == None: continue
                 k = (i+j) % div
-                m_data[k] = max(m_data[k], m+num)
-print(m_data)
-# {0, 1, 2}
-# [3, 13, 5]
-# [6, 7, 14]
-
-# [9, 10, 17] || [27, 19, 20] || [12, 19, 11]
-# [9, 19, 11] || [12, 10, 20] || [27, 19, 17]
-# [27, 19, 20]
+                curr_data[k] = max_with_none(curr_data[k], curr + m)
+    m_data = curr_data
+print(m_data[0] if m_data[0] != None else "NO")
