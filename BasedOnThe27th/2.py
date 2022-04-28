7841ccb0a3141b54ec56f2acc37072997c10fd4a
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-04-28 20:58:05 +0300

    2 fix

--- BasedOnThe27th/2.py
+++ BasedOnThe27th/2.py
@@ -5,27 +5,16 @@ def max_with_none(*a):
     return max(a) if len(a) != 0 else None
 
 div = 15
-data = []
-for string in sys.stdin.read().strip().split('\n'):
-    if string == "": continue
-    table = [None for _ in range(div)]
-    for row in string.split():
-        row = int(row)
-        key = row % div
-        if table[key] == None:
-            table[key] = row
-        else:
-            table[key] = max(row, table[key])
-    data.append(table)
+data = [[int(x) for x in string.split()] for string in sys.stdin.read().split("\n") if string != ""]
 
 m_data = [0] + [None for _ in range(div-1)]
 for string in data:
     curr_data = [None for _ in range(div)]
-    for j, m in enumerate(m_data):
+    for m in m_data:
         if m != None:
-            for i, curr in enumerate(string):
+            for curr in string:
                 if curr == None: continue
-                k = (i+j) % div
+                k = (curr+m) % div
                 curr_data[k] = max_with_none(curr_data[k], curr + m)
     m_data = curr_data
 print(m_data[0] if m_data[0] != None else "NO")
