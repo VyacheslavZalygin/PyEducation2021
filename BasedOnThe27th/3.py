28993db0dadd2fc141139004f5933a50aecbb2ff
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-04-28 22:05:30 +0300

    3 ready :D

--- BasedOnThe27th/3.py
+++ BasedOnThe27th/3.py
@@ -3,29 +3,40 @@ import sys
 div = 15
 data = [[int(x) for x in string.split()] for string in sys.stdin.read().split("\n") if string != ""]
 
-m_data = [[(0, 0), (0, 0)]] + [[None, None] for _ in range(div-1)]
+m_data = [{0: 0}] + [{} for _ in range(div-1)]
 for string in data:
-    curr_data = [[None, None] for _ in range(div)]
-    for m in m_data:
-        for pair in m:
-            if pair != None:
-                even_count, s = pair 
-                for curr in string:
-                    even_count_curr = even_count
-                    k = (curr+s) % div
-                    if curr % 2 == 0: even_count_curr += 1
-                    if curr_data[k][even_count_curr%2] == None or curr_data[k][even_count_curr%2][1] < curr+s:
-                        curr_data[k][even_count_curr%2] = (even_count_curr, curr + s)
+    curr_data = [{} for _ in range(div)]
+    # for m in m_data:
+    #     for pair in m:
+    #         if pair != None:
+    #             e, s = pair 
+    #             for curr in string:
+    #                 even_count = e
+    #                 k = (curr+s) % div
+    #                 if curr % 2 == 0: even_count += 1
+    #                 if curr_data[k][even_count%2] == None or curr_data[k][even_count%2][1] < curr+s:
+    #                     curr_data[k][even_count%2] = (even_count, curr + s)
+
+    for old_table in m_data:
+        for e in old_table:
+            s = old_table[e]
+            for curr in string:
+                even_count = e
+                k = (curr+s) % div
+                new_table = curr_data[k]
+                if curr % 2 == 0: even_count += 1
+                if even_count not in new_table or new_table[even_count] < curr+s:
+                    new_table[even_count] = curr+s
     m_data = curr_data
 
 # print(m_data[0])
 
-choice = []
-if m_data[0][0] != None and m_data[0][0][0] > len(data)//2:
-    choice.append(m_data[0][0][1])
-if m_data[0][1] != None and m_data[0][1][0] > len(data)//2:
-    choice.append(m_data[0][1][1])
-if len(choice) != 0:
-    print(max(choice))
+res = [m_data[0][x] for x in m_data[0] if x > len(data)//2]
+if len(res) != 0:
+    print(max(res))
 else:
     print("NO")
+
+# 1 2 3
+# 12 29
+# 1 16
